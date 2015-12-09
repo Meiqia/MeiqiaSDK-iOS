c30dce9dd476b57b868e60a3f5be42f7c6d479af
ijinmao <340052204@qq.com>
2015-12-09 16:20:40 +0800

    [update] update demo

---
+++ Meiqia-SDK-Demo/Meiqia-SDK-Demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Meiqia-SDK-Demo
+//
+//  Created by ijinmao on 15/12/9.
+//  Copyright © 2015年 ijinmao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
