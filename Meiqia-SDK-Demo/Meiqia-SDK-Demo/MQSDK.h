70a7c8d7bd072b71df973ceb07760f47d4cfdcb2
InjoyDeng <Injoy@DengInjoydeMacBook-Pro.local>
2015-12-29 17:29:40 +0800

    「add」Pods

---
+++ Meiqia-SDK-Demo/Meiqia-SDK-Demo/MQSDK.h
@@ -0,0 +1,12 @@
+//
+//  MQSDK.h
+//  Meiqia-SDK-Demo
+//
+//  Created by Injoy on 15/12/29.
+//  Copyright © 2015年 ijinmao. All rights reserved.
+//
+
+#ifdef __OBJC__
+#import <MeiQiaSDK/MQManager.h>
+#import "MQChatViewManager.h"
+#endif
