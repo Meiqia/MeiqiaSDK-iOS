eebd88c27f8a8b3cadebbeb6f324ab10ae79b064
ijinmao <340052204@qq.com>
2016-01-04 11:11:22 +0800

    [update] update 3.0.4 version

--- Meiqia-SDK-Demo/Meiqia-SDK-Demo/AppDelegate.m
+++ Meiqia-SDK-Demo/Meiqia-SDK-Demo/AppDelegate.m
@@ -37,6 +37,7 @@
     //main
 #error 请填写您的美洽AppKey
     [MQManager initWithAppkey:@"开发者的美洽AppKey" completion:^(NSString *clientId, NSError *error) {
+        NSLog(@"初始化成功");
     }];
     
     return YES;
