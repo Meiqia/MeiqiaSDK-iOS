772005f274d0d9aa8c4db0877c8ec99ba35a22e4
ijinmao <340052204@qq.com>
2016-02-23 12:23:31 +0800

    [fix] 清除系统导航栏 delegate

--- Meiqia-SDK-files/MQChatViewController/Controllers/MQChatViewController.m
+++ Meiqia-SDK-files/MQChatViewController/Controllers/MQChatViewController.m
@@ -124,6 +124,7 @@ static CGFloat const kMQChatViewInputBarHeight = 50.0;
 }
 
 - (void)removeDelegateAndObserver {
+    self.navigationController.delegate  = nil;
     chatViewService.delegate = nil;
     tableDataSource.chatCellDelegate = nil;
     self.chatTableView.chatTableViewDelegate = nil;
@@ -455,6 +456,7 @@ static CGFloat const kMQChatViewInputBarHeight = 50.0;
     if ([navigationController isKindOfClass:[UIImagePickerController class]]) {
         [UIApplication sharedApplication].statusBarStyle = currentStatusBarStyle;
     }
+    self.navigationController.delegate  = nil;
 }
 
 #pragma MQChatCellDelegate
