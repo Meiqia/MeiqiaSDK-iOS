26d3087fd97a9632f2d710939311bc1b93a0e845
Luo Xu <ianluo63@gmail.com>
2016-04-12 17:03:50 +0800

    修复MQChatViewService中没有移除notificatino observer造成崩溃的问题。

--- Meiqia-SDK-files/MQChatViewController/Controllers/MQChatViewService.m
+++ Meiqia-SDK-files/MQChatViewController/Controllers/MQChatViewService.m
@@ -76,8 +76,12 @@ typedef NS_ENUM(NSUInteger, MQClientStatus) {
     return self;
 }
 
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
 - (void)socketStatusChanged:(NSNotification *)notification {
-    static BOOL shouldHandleSocketConnectNotification = NO; //当第一次进入的时候，会受到 socket 连上的消息，但是这个时候并不应该执行重新上线的逻辑，重新上线的逻辑必须是 socket 断开之后才有必要去执行的，这个标志的作用就是在 socket 有过断开的情况才去执行。
+    static BOOL shouldHandleSocketConnectNotification = NO; //当第一次进入的时候，会收到 socket 连上的消息，但是这个时候并不应该执行重新上线的逻辑，重新上线的逻辑必须是 socket 断开之后才有必要去执行的，这个标志的作用就是在 socket 有过断开的情况才去执行。
     if ([[notification.userInfo objectForKey:MQ_NOTIFICATION_SOCKET_STATUS_CHANGE] isEqualToString:SOCKET_STATUS_CONNECTED] && shouldHandleSocketConnectNotification) {
         [self setClientOnline];
         shouldHandleSocketConnectNotification = NO;
