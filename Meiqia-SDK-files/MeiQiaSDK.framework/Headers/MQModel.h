f7ea0ce7ef58b19487fd4a76e332c799ec5e3bea
Luo Xu <ianluo63@gmail.com>
2017-04-06 17:28:07 +0800

    [update] framework

--- Meiqia-SDK-files/MeiQiaSDK.framework/Headers/MQModel.h
+++ Meiqia-SDK-files/MeiQiaSDK.framework/Headers/MQModel.h
@@ -8,10 +8,17 @@
 
 #import <Foundation/Foundation.h>
 
+/// 使用 MQModel 的对象需要实现的方法
+/// 映射的对象包含 3 种
+/// 1. 本对象的属性
+/// 2. 一个带有返回值，返回给指定属性的方法（结尾用 >:属性名）
+/// 3. 一个没有返回值的方法（结尾用 >:）
 @protocol MQMappable <NSObject>
 
+/// 返回服务端 json 字段到本对象字段的映射
 - (NSDictionary *)fromServerMapping;
 
+/// 返回本对象字段到数据库字段的映射
 - (NSDictionary *)toDBMapping;
 
 @optional
