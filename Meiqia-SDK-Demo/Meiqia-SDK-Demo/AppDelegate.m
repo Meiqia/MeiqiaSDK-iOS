aebf1f41870b2597a3495388fc85cc8c3d7e7037
xulianpeng <xulianpeng@haodf.com>
2017-09-08 16:37:53 +0800

    上线前准备

--- Meiqia-SDK-Demo/Meiqia-SDK-Demo/AppDelegate.m
+++ Meiqia-SDK-Demo/Meiqia-SDK-Demo/AppDelegate.m
@@ -37,11 +37,7 @@
     
 //#error 请填写您的美洽 AppKey
     
-    //测试 a891aac118366700931f3df3181c64a0
-    //575679c246573824c8957c0958bee3f1
-    //782a09d4b2e2d20a9eceef75fb45245e
-    
-    [MQManager initWithAppkey:@"782a09d4b2e2d20a9eceef75fb45245e" completion:^(NSString *clientId, NSError *error) {
+    [MQManager initWithAppkey:@"" completion:^(NSString *clientId, NSError *error) {
         if (!error) {
             NSLog(@"美洽 SDK：初始化成功");
         } else {
