79e6b4c09140136c5e2c6d9ad15444c4e7327aa5
xulianpeng <xulianpeng@meiqia.com>
2018-11-09 15:43:12 +0800

    [add]change file position

--- Meiqia-SDK-Demo/Meiqia-SDK-Demo/AppDelegate.m
+++ Meiqia-SDK-Demo/Meiqia-SDK-Demo/AppDelegate.m
@@ -35,7 +35,7 @@
 #endif
     
 #pragma mark  集成第一步: 初始化,  参数:appkey  //0be5c8f39d8959c7f70aab0949b5d3ab 009c6d3b9af54a81653c1982a1425e7b
-    [MQManager initWithAppkey:@"0be5c8f39d8959c7f70aab0949b5d3ab" completion:^(NSString *clientId, NSError *error) {
+    [MQManager initWithAppkey:@"009c6d3b9af54a81653c1982a1425e7b" completion:^(NSString *clientId, NSError *error) {
 
         if (!error) {
             NSLog(@"美洽 SDK：初始化成功");
